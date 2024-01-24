e11be9df81baf562dbe89d272945221c9caf0718
Vivek Pathak <76717203+vivekpathak4543@users.noreply.github.com>
2024-01-24 15:44:53 +0530

    Update main.jsx
    home edit

--- src/main.jsx
+++ src/main.jsx
@@ -35,7 +35,7 @@ import User from './components/User/User.jsx'
 const router =createBrowserRouter(
   createRoutesFromElements(
     <Route path ="/" element ={<Layout />} >
-      <Route path = "home" element ={<Home />} />
+      <Route path = "" element ={<Home />} />
       <Route path ="about" element = {<About />}/>
       <Route path ="contact" element = {<Contact />}/>
       <Route path ="user/:userid" element = {<User />}/>
